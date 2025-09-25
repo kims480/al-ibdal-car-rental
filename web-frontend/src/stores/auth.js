3329114852279725d049f5ee4c2d95553b653a22
Kims480 <eng.karim2010@gmail.com>
2025-09-26 00:39:12 +0400

    SR Update1

--- web-frontend/src/stores/auth.js
+++ web-frontend/src/stores/auth.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api'
+const API_BASE_URL = import.meta.env.VITE_API_URL
 
 // Configure axios
 axios.defaults.baseURL = API_BASE_URL
